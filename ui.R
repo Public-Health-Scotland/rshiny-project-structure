60000aff5b5af7d5812585a70e552a82a33a719b
jvillacampa <30340183+jvillacampa@users.noreply.github.com>
2018-07-02 16:51:22 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,8 @@
+# Description - Describe what the app does (e.g. visualizes births data)
+# User interface - how your app looks and elements users can interact with
 
+fluidPage(
+  
+) # fluidPage bracket
+
+## END
