52877b435abdcd0e98fc906bf5c9531cb512b632
jvillacampa <30340183+jvillacampa@users.noreply.github.com>
2018-07-02 16:49:18 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,1 +1,9 @@
+# Description - Describe what the app does (e.g. visualizes births data)
+# This is the server script, it produces the outputs and reactive objects
+# of the app: charts, text,
 
+function(input, output, session) {
+  
+}
+
+## END
