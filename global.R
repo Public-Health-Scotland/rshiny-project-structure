423a6dbde39769f9eb284d93fe3adc3bfc145368
jvillacampa <30340183+jvillacampa@users.noreply.github.com>
2018-07-02 16:55:17 +0100

    Update global.R

--- global.R
+++ global.R
@@ -1,1 +1,22 @@
+# Description - Describe what the app does (e.g. visualizes births data)
+# this section includes the non-reactive elements and everything used by both the
+# UI and Server sides: functions, packages, data, etc.
 
+############################.
+##Packages ----
+############################.
+library(shiny)
+
+###############################################.
+## Functions ----
+###############################################.  
+
+###############################################.
+## Data ----
+###############################################.    
+
+###############################################.
+## Palettes ----
+###############################################.   
+
+## END
